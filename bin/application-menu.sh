11edde43c17140cf01c8f2be65b9eb80227b8f76
nxvxl <nxvxl@protonmail.com>
2021-07-03 10:37:55 +0700

    switch to i3blocks

---
+++ bin/application-menu.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+rofi \
+	-modi drun \
+	-show drun \
+	-show-icons \
+	-fullscreen \
+	-padding 240 \
+	-font 'JetBrainsMono 16' \
+	-color-window '#cc242424,#000000,#dedede' \
+	-color-normal '#00242424,#dedede,#00242424,#0860f2,#ffffff'
+
