27d707d8e529c2e9131fbd1c061793f7ce0e8ee4
noval <nxvxl@protonmail.com>
2021-11-14 21:43:06 +0700

    search word under cursor using telescope

--- nvim-nightly/.config/nvim/lua/plugin-config/telescope.lua
+++ nvim-nightly/.config/nvim/lua/plugin-config/telescope.lua
@@ -1,7 +1,6 @@
 local actions = require('telescope.actions')
 require('telescope').setup{
   defaults = {
-    -- file_sorter = require('telescope.sorters').get_fzy_sorter,
     file_ignore_patterns = { 'node_modules/*', '.git/*' },
     layout_config = {
       prompt_position = "top"
@@ -12,14 +11,25 @@ require('telescope').setup{
         ["<esc>"] = actions.close
       },
     },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden"
+    }
   },
 }
 
 local map = require('utils').map
 
 map('n', '<leader>ff', ':lua require"telescope.builtin".find_files({ hidden = true })<CR>')
-map('n', '<leader>fg', ':lua require"telescope.builtin".live_grep{ hidden = true } <CR>')
+map('n', '<leader>fg', ':lua require"telescope.builtin".live_grep() <CR>')
 map('n', '<leader>fb', ':Telescope buffers<CR>')
 map('n', '<leader>fh', ':Telescope help_tags<CR>')
 map('n', '<leader>\\', ':Telescope<CR>')
 map('n', 'gr', ':Telescope lsp_references<CR>')
+map('n', '<leader>fG', ':Telescope grep_string search=<C-R><C-W><CR>')
