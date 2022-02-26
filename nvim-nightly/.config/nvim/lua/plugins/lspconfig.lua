7f98dd4fd78f556bcbfd297e4e17658a80083784
noval <noval.dev@protonmail.com>
2022-02-26 16:14:56 +0700

    add vuels and cssls

--- nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
@@ -49,7 +49,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "tsserver", "pyright", "clangd" }
+local servers = { "tsserver", "pyright", "clangd", "vuels", "cssls" }
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
