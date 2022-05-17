602add21aac0eabe68e0008963a86fff64cfe859
noval <noval.dev@protonmail.com>
2022-05-17 21:16:02 +0700

    update lspconfig; replace resolved_capabilities with server_capabilities

--- nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
@@ -24,14 +24,14 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
+  if client.server_capabilities.documentFormattingProvider then
+    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.format({ async = true })<CR>", opts)
+  elseif client.server_capabilities.documentRangeFormattingProvider then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
   -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.documentHighlightProvider then
     vim.api.nvim_exec([[
       hi LspReferenceRead cterm=bold ctermbg=red guibg=#44475a
       hi LspReferenceText cterm=bold ctermbg=red guibg=#44475a
