46039d3593af895c11712cfd25febf810cbb1d47
noval <noval.dev@protonmail.com>
2022-05-28 10:36:08 +0700

    add html language server

--- nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim-nightly/.config/nvim/lua/plugins/lspconfig.lua
@@ -49,7 +49,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "tsserver", "pyright", "clangd", "vuels", "cssls", "gopls" }
+local servers = { "tsserver", "pyright", "clangd", "vuels", "cssls", "gopls", "html" }
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
