795c3ec30680b456c3e7b7c2409433cbfb3d218f
noval <nxvxl@protonmail.com>
2023-02-22 21:04:42 +0700

    fix gitsigns config

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,12 +1,15 @@
--- Gitsigns
--- See `:help gitsigns.txt`
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
+local nmap = require('utils').nmap
 
+require('gitsigns').setup({
+  signs = {
+    add = { hl = 'GitGutterAdd', text = '▋' },
+    change = { hl = 'GitGutterChange', text = '▋' },
+    delete = { hl = 'GitGutterDelete', text = '▋' },
+    topdelete = { hl = 'GitGutterDeleteChange', text = '▔' },
+    changedelete = { hl = 'GitGutterChange', text = '▎' },
+  },
+  on_attach = function()
+    nmap('[c', ':Gitsigns prev_hunk<CR>')
+    nmap(']c', ':Gitsigns next_hunk<CR>')
+  end
+})
