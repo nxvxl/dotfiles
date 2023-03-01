14e7b0e1d0ba745be9c516c5403fee7e78d4fed2
noval <nxvxl@protonmail.com>
2023-03-01 21:27:49 +0700

    lock on suspend

--- nvim/.config/nvim/lua/plugins/custom/finder.lua
+++ nvim/.config/nvim/lua/plugins/custom/finder.lua
@@ -49,7 +49,9 @@ return {
         })
       end, { desc = '[/] Fuzzily search in current buffer]' })
 
-      vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        require('telescope.builtin').find_files({ hidden = true })
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string,
         { desc = '[S]earch current [W]ord' })
