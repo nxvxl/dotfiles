37978013d5c2ab71464364e0accdcea624959d73
noval <nxvxl@protonmail.com>
2024-06-28 17:57:17 +0700

    update tag.sh script

--- bin/tag.sh
+++ bin/tag.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 #
 TAG="prd-$(date +%Y%m%d)"
+LAST_TAG=$(git describe --tags --abbrev=0)
+MESSAGE=$(git log $(git describe --tags --abbrev=0)..HEAD --merges | grep "Merge branch" | cut -d " " -f7 | uniq | grep -v "master" | sort | sed "s/'//g" | sed "s/\r//g")
 
-git tag $TAG
+if [[ "$TAG" == "$LAST_TAG" ]]; then
+  if [[ "$1" == "-f" ]]; then
+    git tag --delete "$LAST_TAG"
+    git push --delete origin "$LAST_TAG"
+  else
+    echo "latest tag already: $TAG"
+    exit 0
+  fi
+fi
+
+git tag -a -m "$MESSAGE" $TAG
 git push origin $TAG
 
 echo $TAG
+echo $MESSAGE
