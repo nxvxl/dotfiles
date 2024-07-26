5e6775e1ecf9d13625bfd9230d13bdf9c6a417d7
noval <nxvxl@protonmail.com>
2024-07-26 10:09:47 +0700

    rofi: add vpn selector

--- bin/action-control.py
+++ bin/action-control.py
@@ -8,11 +8,12 @@ OPTIONS = {
     "Switch Audio Output": f"{HOME}/.dotfiles/bin/speaker-selector.sh",
     "Toggle Notification": "dunstctl set-paused toggle && pkill -SIGRTMIN+12 i3blocks",
     "Mic Selector": f"{HOME}/.dotfiles/bin/mic-selector.sh",
-    "Emoji": f"{HOME}/.dotfiles/bin/emoji-selector.sh"
+    "Emoji": f"{HOME}/.dotfiles/bin/emoji-selector.sh",
+    "VPN": f"{HOME}/.dotfiles/bin/rofi-vpn.sh"
 }
 KEYS = "\n".join(OPTIONS.keys())
 
-result = subprocess.run(["rofi", "-dmenu", "-p", "🎮"],
+result = subprocess.run(["rofi", "-dmenu", "-p", "🎮", "-i"],
                         stdout=subprocess.PIPE,
                         input=KEYS,
                         encoding="ascii")
