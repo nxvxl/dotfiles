608040d74eff7db46f4f91b9c94fa351bb3a2b20
noval <nxvxl@protonmail.com>
2024-07-28 09:52:23 +0700

    fish: remove history bind

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,8 +8,6 @@ set __fish_git_prompt_show_informative_status 1
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showuntrackedfiles 1
 
-
-bind \ch h
 bind -k nul workspace
 
 if test -f ~/.asdf/asdf.fish
