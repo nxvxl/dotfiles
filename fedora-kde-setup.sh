929e2f5f243cb498854be325329ddb5a4ae4d341
noval <nxvxl@protonmail.com>
2025-10-16 11:35:04 +0700

    install dnf-plugins-core

--- fedora-kde-setup.sh
+++ fedora-kde-setup.sh
@@ -20,7 +20,8 @@ install_packages() {
   sudo dnf install -y \
     "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
     "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" \
-    fedora-workstation-repositories
+    fedora-workstation-repositories \
+    dnf-plugins-core
 
   heading "Enabling external repositories (Copr, Docker)"
   sudo dnf copr enable -y jdxcode/mise
