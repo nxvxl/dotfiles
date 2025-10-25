720bf5e27b123f0c6389dc057d5b485bab3481c3
noval <nxvxl@protonmail.com>
2025-10-25 21:04:12 +0700

    update

--- fish/.config/fish/functions/workspace.fish
+++ fish/.config/fish/functions/workspace.fish
@@ -1,6 +1,6 @@
 function workspace
   set -l DIR ~/Works/Vouch/Repository
-  set -l SELECTED (find "$DIR" -mindepth 1 -maxdepth 1 -type d | xargs basename -a | fzf-tmux -p)
+  set -l SELECTED (find "$DIR" -mindepth 1 -maxdepth 1 -type d | xargs basename -a | fzf)
 
   if test "$SELECTED"
     cd "$DIR/$SELECTED"
