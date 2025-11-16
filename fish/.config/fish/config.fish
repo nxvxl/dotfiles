df0a2b697259600a8d563e7310a063299eac1f43
noval <nxvxl@protonmail.com>
2025-11-16 10:30:37 +0700

    update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,7 +8,7 @@ set __fish_git_prompt_show_informative_status 1
 set __fish_git_prompt_showdirtystate 1
 set __fish_git_prompt_showuntrackedfiles 1
 
-bind ctrl+space workspace
+bind ctrl-space workspace
 
 if test -f ~/.asdf/asdf.fish
   source ~/.asdf/asdf.fish
