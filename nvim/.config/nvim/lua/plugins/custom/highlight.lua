c7007a41bd6b569fc55ed966bad4ebfa5f2995d5
noval <nxvxl@protonmail.com>
2026-06-09 08:02:37 +0700

    update treesitter

--- nvim/.config/nvim/lua/plugins/custom/highlight.lua
+++ nvim/.config/nvim/lua/plugins/custom/highlight.lua
@@ -5,70 +5,19 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    build = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
+    branch = 'main',
     config = function()
-      require('nvim-treesitter.configs').setup {
-        -- Add languages to be installed here that you want installed for treesitter
-        -- ensure_installed = { 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'vim', 'tsx' },
-
-        highlight = { enable = true },
-        indent = { enable = true, disable = { 'python' } },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = '<c-space>',
-            node_incremental = '<c-space>',
-            scope_incremental = '<c-s>',
-            node_decremental = '<c-backspace>',
-          },
-        },
-        -- textobjects = {
-        --   select = {
-        --     enable = true,
-        --     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        --     keymaps = {
-        --       -- You can use the capture groups defined in textobjects.scm
-        --       ['aa'] = '@parameter.outer',
-        --       ['ia'] = '@parameter.inner',
-        --       ['af'] = '@function.outer',
-        --       ['if'] = '@function.inner',
-        --       ['ac'] = '@class.outer',
-        --       ['ic'] = '@class.inner',
-        --     },
-        --   },
-        --   move = {
-        --     enable = true,
-        --     set_jumps = true, -- whether to set jumps in the jumplist
-        --     goto_next_start = {
-        --       [']m'] = '@function.outer',
-        --       [']]'] = '@class.outer',
-        --     },
-        --     goto_next_end = {
-        --       [']M'] = '@function.outer',
-        --       [']['] = '@class.outer',
-        --     },
-        --     goto_previous_start = {
-        --       ['[m'] = '@function.outer',
-        --       ['[['] = '@class.outer',
-        --     },
-        --     goto_previous_end = {
-        --       ['[M'] = '@function.outer',
-        --       ['[]'] = '@class.outer',
-        --     },
-        --   },
-        --   swap = {
-        --     enable = true,
-        --     swap_next = {
-        --       ['<leader>a'] = '@parameter.inner',
-        --     },
-        --     swap_previous = {
-        --       ['<leader>A'] = '@parameter.inner',
-        --     },
-        --   },
-        -- },
-      }
+      require('nvim-treesitter').setup()
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          -- Enable treesitter highlighting and disable regex syntax
+          pcall(vim.treesitter.start)
+          -- Enable treesitter-based indentation
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end
   },
   -- {
